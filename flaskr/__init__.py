40566cf6ee70714b84e88d6d5388750c73526ac7
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2022-08-02 10:20:57 +0200

    add cpu temp endpoint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,7 +4,8 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
-    from . import ping
+    from . import ping, cpu_temp
     app.register_blueprint(ping.bp)
+    app.register_blueprint(cpu_temp.bp)
 
     return app
