c9002acdbff564524996f2d99fd7497bfebe773c
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2022-08-03 14:38:52 +0200

    delete assert test and add ping test

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -1,3 +1,15 @@
-def test_ping(client):
+import pytest
+
+from flaskr.ping import ping
+
+
+@pytest.mark.unit
+def test_ping():
+    response = ping()
+    assert 'ping' == response
+
+
+@pytest.mark.integration
+def test_ping_call(client):
     response = client.get('/ping')
     assert b"ping" in response.data
